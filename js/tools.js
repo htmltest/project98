a413ab35b186850822df6d3cc8a2702e91ebb73b
htmltest <struev.vs@gmail.com>
2017-12-26 16:26:56 +0300

    updates

--- js/tools.js
+++ js/tools.js
@@ -14,6 +14,32 @@ $(document).ready(function() {
         } else {
             $('nav').removeClass('fixed');
         }
+
+        $('nav a').each(function() {
+            var curBlock = $(this).attr('href');
+            if ($(curBlock).length > 0 && $(curBlock).offset().top < (curScroll + $(window).height())) {
+                $('nav li.active').removeClass('active');
+                $(this).parent().addClass('active');
+            }
+        });
+        var curWidth = $('nav ul').offset().left;
+        if ($('nav li.active').length > 0) {
+            curWidth = $('nav li.active').offset().left + $('nav li.active').width();
+        }
+        $('.nav-line').css({'width': curWidth});
+    });
+
+    $('.up-link').click(function(e) {
+        $.scrollTo(0, {duration : 500});
+        e.preventDefault();
+    });
+
+    $(window).on('load resize scroll', function() {
+        if ($(window).scrollTop() > $(window).height()) {
+            $('.up-link').addClass('visible');
+        } else {
+            $('.up-link').removeClass('visible');
+        }
     });
 
 });
